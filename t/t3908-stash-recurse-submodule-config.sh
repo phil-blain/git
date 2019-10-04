3f60e0fc7985fc4f8e7790deb05bb0a1b08242ac
Philippe Blain <levraiphilippeblain@gmail.com>
2019-10-03 22:48:21 -0400

    stash: add t/t3908 demonstrating a failure with submodules
    The new test t/t3908 demonstrates a case where 'git stash'
    will irreversibly lose changes to modified submodule files
    in a project with a submodule.
    
    When the configuration option recurse.submodule is set to true,
    'git stash' internally calls 'git reset --hard'
    with the '--recurse-submodules' flag (implicitly).
    If there are modified files in the submodule,
    the changes will be lost as the submodule is reset to the commit
    recorded in the superproject.
    
    Signed-off-by: Philippe Blain <levraiphilippeblain@gmail.com>

---
+++ t/t3908-stash-recurse-submodule-config.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+test_description='stash does not touch submodules even if submodule.recurse=true'
+
+. ./test-lib.sh
+
+test_expect_success 'setup' '
+	test_create_repo submodule &&
+	test_commit -C submodule first first &&
+	test_create_repo project &&
+	git -C project submodule add ../submodule &&
+	git -C project add submodule &&
+	test_tick &&
+	git -C project commit -m add_sub &&
+	test_commit -C project first.super first.super &&
+	date >project/submodule/first &&
+	date >project/first.super
+'
+
+test_expect_failure 'stash in superproject does not touch modified submodule files if submodule.recurse=true' '
+	git -C project/submodule status >>expect &&
+	git -C project -c submodule.recurse=true stash &&
+	git -C project/submodule status >>actual &&
+	test_cmp expect actual
+'
+
+test_done
