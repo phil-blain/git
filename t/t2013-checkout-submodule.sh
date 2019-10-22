602ae354d4e20599066664645718210d6cdcdf75
Philippe Blain <levraiphilippeblain@gmail.com>
2019-10-22 00:38:02 -0400

    checkout: add failing test with initialized nested submodules
    Switching between a branch without submodules and a branch
    with initialized nested submodules fails.
    
    The command fails when read-tree tries to run in the nested
    submodule:
    
    00:44:49.910292 run-command.c:663       trace: run_command: cd nested;
    unset GIT_INTERNAL_SUPER_PREFIX GIT_PREFIX; GIT_DIR=.git git
    --super-prefix=sub-nested/nested/ read-tree --recurse-submodules -n
    --reset 599f73865fc6074b894548aba11ce4079cb38e4c
    fatal: exec '--super-prefix=sub-nested/nested/': cd to 'nested' failed:
    No such file or directory
    error: Submodule 'nested' could not be updated.
    error: Submodule 'sub-nested/nested' cannot checkout new HEAD.
    error: Submodule 'sub-nested' could not be updated.

--- t/t2013-checkout-submodule.sh
+++ t/t2013-checkout-submodule.sh
@@ -72,4 +72,31 @@ test_submodule_switch "git checkout"
 
 test_submodule_forced_switch "git checkout -f"
 
+test_expect_success 'setup initialized nested submodules in a branch' '
+	test_create_repo sub-nested &&
+	test_commit -C sub-nested first &&
+	test_create_repo nested &&
+	test_commit -C nested first &&
+	git -C sub-nested submodule add ../nested &&
+	git -C sub-nested add nested &&
+	test_tick &&
+	git -C sub-nested commit -m "add nested" &&
+	test_create_repo project &&
+	test_commit -C project first &&
+	git -C project checkout -b add-sub &&
+	git -C project submodule add ../sub-nested &&
+	git -C project add sub-nested &&
+	test_tick &&
+	git -C project commit -m "add sub" &&
+	git -C project submodule update --init --recursive
+'
+
+test_expect_failure 'checkout --recurse-submodules between branches with and without initialized nested submodules' '
+	git -C project status >expect &&
+	git -C project checkout --recurse-submodules master &&
+	git -C project checkout --recurse-submodules add-sub &&
+	git -C project status >actual &&
+	test_cmp expect actual
+'
+
 test_done
