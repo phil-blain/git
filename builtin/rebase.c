468682a7c0656e69878230ff211bf5edee7f9d99
Philippe Blain <levraiphilippeblain@gmail.com>
2021-08-13 12:51:24 -0400

    builtin/rebase: honor submodule.recurse
    This works because it sets a global variable in submodule.c which is
    read by unpack_trees.

--- builtin/rebase.c
+++ builtin/rebase.c
@@ -29,6 +29,7 @@
 #include "rerere.h"
 #include "branch.h"
 #include "sequencer.h"
+#include "submodule.h"
 #include "rebase-interactive.h"
 #include "reset.h"
 #include "trace2.h"
@@ -805,6 +806,9 @@ static int rebase_config(const char *var, const char *value,
 	if (!strcmp(var, "rebase.backend")) {
 		return git_config_string(&opts->default_backend, var, value);
 	}
+
+	if (!strcmp(var, "submodule.recurse"))
+		return git_default_submodule_config(var, value, data);
 
 	return git_default_config(var, value, ctx, data);
 }
