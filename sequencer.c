0bcf9c9e378b0a6dec37d986b70c50e20cc8e82e
Philippe Blain <levraiphilippeblain@gmail.com>
2024-01-19 16:16:45 -0500

    sequencer: honor submodule.recurse
    Signed-off-by: Philippe Blain <levraiphilippeblain@gmail.com>

--- sequencer.c
+++ sequencer.c
@@ -48,6 +48,7 @@
 #include "rebase-interactive.h"
 #include "reset.h"
 #include "branch.h"
+#include "submodule.h"
 
 #define GIT_REFLOG_ACTION "GIT_REFLOG_ACTION"
 
@@ -325,6 +326,9 @@ static int git_sequencer_config(const char *k, const char *v,
 
 	if (opts->action == REPLAY_REVERT && !strcmp(k, "revert.reference"))
 		opts->commit_use_reference = git_config_bool(k, v);
+
+	if (!strcmp(k, "submodule.recurse"))
+                return git_default_submodule_config(k, v, cb);
 
 	return git_diff_basic_config(k, v, ctx, NULL);
 }
